87bab99ee2973b0df34ecef13f7d77a3d4f57d21
Henry Casey <hpc3@njit.edu>
2020-03-31 17:01:30 -0400

    Modified: SearchContainer handles a users loggedIn state which conditionally renders out the buttons to modify produce's quantity

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,47 +1,65 @@
-import React from 'react';
-import axios from 'axios';
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
+import firebase from './firebase'
 
 import SearchTools from "./SearchTools";
 import Produce from "./Produce";
+import Login from './Login';
 
 import '../componentStyles/SearchContainer.css';
 
-
-
-class SearchContainer extends React.Component{
+class SearchContainer extends Component{
 
     constructor(props){
         super(props)
-
         this.state = {
             produce: [],
             error: '',
+            loggedIn: false
         }
-
         this.sortList = this.sortList.bind(this);
+        this.handleUserState = this.handleUserState.bind(this);
     }
-
 
     componentDidMount() {
-        this.getProduceDate();
+        this.getProduceFromFirestore();
     }
 
-    getProduceDate(){
-        axios.get('http://my-json-server.typicode.com/hpc3/StandDB/produce')
-            .then(response => {
-                this.setState({produce: response.data});
-            }).catch(error => {
-            this.setState({errorMessage: error.message});
-        });
+    handleUserState(){
+        this.setState((prevState) => ({
+            loggedIn: !prevState.loggedIn
+        }))
     }
 
-    
+    getProduceFromFirestore (){
+        firebase
+            .firestore()
+            .collection('produce')
+            .get()
+            .then(querySnapshot => {
+                let tempArray = [];
+                console.log(querySnapshot)
+                querySnapshot.forEach(doc => {
+                    // grab doc.data() and set it to temp obj
+                    let tempObj = doc.data();
+                    //grab the id and add it to temp obj
+                    tempObj['id'] = doc.id;
+                    tempArray.push(tempObj)
+                })
+                this.setState({
+                    produce: tempArray
+                })
+            })
+    }
 
     sortList(e){
         if (e.target.value === 'a-z'){
-            //Loops through an array of objects, p.id
-
+                //Loops through an array of objects, p.id
                 const tempArray = this.state.produce.sort((a, b) => {
                    if (b.name > a.name) return -1;
                    if (b.name < a.name) return 1;
@@ -85,18 +103,29 @@ class SearchContainer extends React.Component{
     }
     render() {
         return(
-            <div className='SearchContainer' id='produce'>
+            <div className='SearchContainer' id="produce" >
+                <Router>
+                    <Switch>
+                        <Route path="/Dad">
+                            <Login
+                                loginState={this.state.loggedIn}
+                                loginHandler={this.handleUserState}
+                            />
+                        </Route>
+                    </Switch>
+                </Router>
                 <SearchTools sortList={this.sortList}/>
                 <div className='produce-container'>
                     {this.state.produce.map((produce, index) =>
                         <Produce
-                            {...produce}
-                            key={produce.id}
-                            index={index}
+                        {...produce}
+                        key={produce.id}
+                        index={index}
+                        loggedIn={this.state.loggedIn}
                         />
-                    )}
+                     )}
                 </div>
-            </div>
+            </div> 
         )
     }
 }
