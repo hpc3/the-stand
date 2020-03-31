726eb5e8f5f892e775c4793ee5b7aa5207a0bb04
Henry Casey <hpc3@njit.edu>
2020-03-31 16:53:57 -0400

    Modified: App is now a functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
 
-
-import Header from './Header';
-import AboutUs from './AboutUs';
+import Header from './Header'
+import AboutUs from './AboutUs'
 import SearchContainer from './SearchContainer';
 import ContactUs from "./ContactUs";
 
-
 import '../componentStyles/App.css';
 
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-
-
-class App extends React.Component{
-
-    render() {
-        return (
-            <div>
-                <Header/>
-                <AboutUs/>
-                <SearchContainer/>
-                <ContactUs/>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <AboutUs/>
+            <SearchContainer/>
+            <ContactUs/>
+        </div>
+    );
 }
 
 export default App;
