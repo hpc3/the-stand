241ba2b983505d8ded15285a75ceb3a22b2caea8
Henry Casey <hpc3@njit.edu>
2020-04-01 14:39:05 -0400

    Implemented Hashrouter, moved it into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './components/App';
 
 
 
 ReactDom.render(
-    <App />,
+    <HashRouter>
+        <App />
+    </HashRouter>
+    ,
     document.getElementById('root')
 );
