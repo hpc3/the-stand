69091207cf344f71c1f4be099ec21688ec68e5dc
Henry Casey <hpc3@njit.edu>
2020-06-04 13:04:31 -0400

    Modified:
    Moved react-spring up to Header, adjusting the  height of header-wrapper for animation
    
    Implemented useState & useEffect hooks to trigger animation when mounted to DOM
    
    Renamed header background: IMG_1161.jpg -> headerImage.jpg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Spring } from 'react-spring/renderprops';
 
 import HeaderTitle from './HeaderTitle';
 import HeaderNav from "./HeaderNav";
 
-import '../componentStyles/font.css';
 import '../componentStyles/Header.css';
 
-export default function Header(){
-    return(
+
+
+const Header = () => {
+    
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    
+
+    let display;
+
+
+    if(mounted){
+        display =
+                    <Spring
+                        from={{ height: '100%',
+                    opacity: 0 }}
+                        to={{ height: '30%',
+                    opacity: 1 }}
+                        config={{ duration: 1500 }}
+                    >
+                        {props =>
+                                <div style={props} id="header-wrapper">
+                                    <HeaderTitle />
+                                    <HeaderNav />
+                                </div>
+                        }
+                    </Spring>
+    }
+    
+    return (
         <header>
-            <HeaderTitle/>
-            <HeaderNav/>
+            {display}
         </header>
     )
 }
+
+export default Header;
--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,23 +1,19 @@
 import React from "react";
-import {Spring} from 'react-spring/renderprops';
+
 
 import '../componentStyles/Header.css'
 
-export default function HeaderNav() {
+const HeaderNav = () => {
 
     return(
-        <Spring
-            from={{opacity: 0, paddingTop: 25}}
-            to={{opacity: 1, paddingTop: 0}}
-            config={{duration: 1250}}
-        >
-            {props => <div style={props}>
-                <nav className="header-nav">
-                    <li className='header-nav-item'><a href='#about-us'>About Us</a></li>
-                    <li className='header-nav-item'><a href='#produce'>Produce</a></li>
-                    <li className='header-nav-item'><a href='#contact-us'>Contact Us</a></li>
-                </nav>
-            </div>}
-        </Spring>
+        <div>
+            <nav id="header-nav">
+                <li className='header-nav-item'><a href='#about-us'>About Us</a></li>
+                <li className='header-nav-item'><a href='#produce'>Produce</a></li>
+                <li className='header-nav-item'><a href='#contact-us'>Contact Us</a></li>
+            </nav>
+        </div>
     )
 }
+
+export default HeaderNav;
