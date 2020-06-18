5a87a49ae1a6a0b32c4614c3670b90f08bc46f09
Henry Casey <hpc3@njit.edu>
2020-06-18 13:53:25 -0400

    Modified: Removed firebase Imported axios
    Changed this.state.email to this.state.username
    
    Implemented express API to log in user
    
    Condensed form input handler into one function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,100 +1,101 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase';
+import React, { Component } from "react";
+import axios from "axios";
 
-import '../componentStyles/Login.css';
+import "../componentStyles/Login.css";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      errorCode: "",
+      errorMessage: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            errorCode: '',
-            errorMessage: ''
-        }
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleSignIn = this.handleSignIn.bind(this);
-        this.handleLogOut = this.handleLogOut.bind(this);
+
+  handleInputChange = (e) => {
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  handleSignIn = (e) => {
+    e.preventDefault();
+
+    const username = this.state.username;
+    const password = this.state.password;
+
+    let payload = { username, password };
+
+    axios
+      .post("auth/login", payload)
+      .then((res) => {
+        localStorage.setItem("token", res.data);
+        this.props.loginHandler();
+
+        this.setState({
+            username: "",
+            password: "",
+            errorCode: "",
+            errorMessage: ""
+        });
+
+      })
+      .catch((err) => {
+        this.setState({
+          username: "",
+          password: "",
+          errorCode: err.response.status,
+          errorMessage: err.response.data,
+        });
+      });
+  };
+
+  handleLogOut = (e) => {
+    e.preventDefault();
+    this.loginHandler();
+    localStorage.removeItem("token");
+  };
+
+  render() {
+
+
+    const loggedIn = this.props.loginState;
+    let view;
+
+    if (!loggedIn) {
+      view = <button onClick={this.handleSignIn}>Submit</button>;
+    } else {
+      view = <button onClick={this.handleLogOut}>Log Out</button>;
     }
 
-    handleEmailChange = (e) => {
-        const email = e.target.value
-        this.setState({email})
-    }
-
-    handlePasswordChange = (e) => {
-        const password = e.target.value
-        this.setState({password})
-    }
-
-    handleSignIn = (e) => {
-        e.preventDefault();
-
-        const email = this.state.email;
-        const password = this.state.password;
-
-        firebase.auth().signInWithEmailAndPassword(email, password).then(cred => {
-            alert('Logged In');
-            this.setState({loggedIn: true})
-            this.props.loginHandler();
-        }).catch(error => {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            this.setState({
-                email: '',
-                password: '',
-                errorCode,
-                errorMessage
-            })
-            alert('Invalid Login Credentials');
-        })
-
-    }
-
-    handleLogOut = (e) => {
-        e.preventDefault(); 
-        firebase.auth().signOut().then(() => {
-            alert('Logged Out');
-            this.props.loginHandler();
-        }).catch(LogOutError => {
-            console.log(LogOutError);
-        })
-    }
-
-    render() {
-
-        const loggedIn = this.props.loginState;
-        let view;
-
-        if(!loggedIn){
-            view = <button onClick={this.handleSignIn}>Submit</button>
-        }else{
-            view = <button onClick={this.handleLogOut}>Log Out</button>
-        }
-
-        return (
-            <div>
-                <form>
-                    <div id="login-input-wrapper">
-                        <label>Email:</label>
-                        <input 
-                            value={this.state.email}
-                            onChange={this.handleEmailChange}
-                            type='email'/>
-                        <label>Password:</label>
-                        <input
-                            type='password'
-                            onChange={this.handlePasswordChange}
-                            value={this.state.password}
-                        />
-                    </div>
-                    {view}
-                </form>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <form>
+          <div id="login-input-wrapper">
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              onChange={this.handleInputChange}
+              value={this.state.password}
+            />
+          </div>
+          {view}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
