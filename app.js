7fa34a66a872ccc181da4226291ee26ef3fc10f1
Henry Casey <hpc3@njit.edu>
2020-06-18 13:55:21 -0400

    Added: Express App

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+const port = 5000;
+
+const app = express();
+
+
+
+// Import Routes
+
+const authRoutes = require('./routes/auth');
+const produceRoutes = require('./routes/produce');
+const postRoutes = require('./routes/posts');
+
+
+app.use(express.json());
+
+app.use('/posts', postRoutes);
+app.use('/produce', produceRoutes);
+app.use('/auth', authRoutes);
+
+
+// CONNECT TO DATABASE
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {console.log('Connected to MongoDB');
+});
+
+
+// LISTEN
+app.listen(port, () => {
+    console.log(`SERVER IS UP ON PORT ${port}` );
+});
