7f19c31efe53692651dfd977e0383d775cbc4459
Henry Casey <hpc3@njit.edu>
2020-06-22 17:07:13 -0400

    Modified:
    Updated keys to reflects id's of documents

--- client/src/components/images.js
+++ client/src/components/images.js
@@ -7,13 +7,13 @@ import TomatoeBaskets from '../images/tomatoes-basket.jpg'
 import TomatoesLoose from '../images/tomatoes-loose.jpg'
 
 const images = {
-    'Peaches': {src: Peaches},
-    'Beetroot': {src: Beets},
-    'Green Beans (Baskets)': {src: GBeans},
-    'Cucumbers' : {src: Cucs},
-    'Corn': {src: Corn},
-    'Tomatoes (Baskets)': {src: TomatoeBaskets},
-    'Tomatoes (Loose)': {src: TomatoesLoose}
+    'peaches': {src: Peaches},
+    'beetroots': {src: Beets},
+    'green-beans': {src: GBeans},
+    'cucumbers' : {src: Cucs},
+    'corn': {src: Corn},
+    'tomato-basket': {src: TomatoeBaskets},
+    'tomato-loose': {src: TomatoesLoose}
 }
 
 export default images;
