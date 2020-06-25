66e9321bbcf62aebd0ecd0da82a1acd638931b74
Henry Casey <hpc3@njit.edu>
2020-06-25 12:28:01 -0400

    Bug Fix:
    Check if Authorization was sent in header of request

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -6,15 +6,22 @@ const jwt = require("jsonwebtoken");
 function verifyToken(req, res, next) {
 
   try{
+
     const bearerHeader = req.headers["authorization"];
+
+    if(bearerHeader === undefined){
+      throw Error("No token given, must be signed in to make this request");
+    }
+
     const token = bearerHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
     next();
     return;
-
+ 
   }
   catch(error){
-    res.json(error);
+    next(error);
+    return;
   }
 
 }
