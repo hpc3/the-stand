fbc2416f89c965165d7f34010463d4caa04cb773
Henry Casey <hpc3@njit.edu>
2020-06-25 17:44:02 -0400

    Added:
    Error Handler

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv/config');
+require("dotenv/config");
 
 const port = 5000;
 
 const app = express();
 
-
-
 // Import Routes
 
-const authRoutes = require('./routes/auth');
-const produceRoutes = require('./routes/produce');
-const commentRoutes = require('./routes/comments');
-
+const authRoutes = require("./routes/auth");
+const produceRoutes = require("./routes/produce");
+const commentRoutes = require("./routes/comments");
 
 app.use(express.json());
 
-app.use('/comment', commentRoutes);
-app.use('/produce', produceRoutes);
-app.use('/auth', authRoutes);
+app.use("/comment", commentRoutes);
+app.use("/produce", produceRoutes);
+app.use("/auth", authRoutes);
 
+// ERROR HANDLER
+
+app.use((error, req, res, next) => {
+  res.status(error.status);
+  res.json({
+    message: error.message,
+  });
+});
 
 // CONNECT TO DATABASE
 
 mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {console.log('Connected to MongoDB');
-});
-
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 // LISTEN
 app.listen(port, () => {
-    console.log(`SERVER IS UP ON PORT ${port}` );
+  console.log(`SERVER IS UP ON PORT ${port}`);
 });
